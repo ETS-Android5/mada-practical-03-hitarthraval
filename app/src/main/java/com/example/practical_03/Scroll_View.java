ff5375baa17533a76fe1f1d9dcef774c93299999
hitarthraval <hitarthraval.hr@gmail.com>
2022-02-27 19:55:07 +0530

    Submission of Practical 03

---
+++ app/src/main/java/com/example/practical_03/Scroll_View.java
@@ -0,0 +1,13 @@
+package com.example.practical_03;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Scroll_View extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
